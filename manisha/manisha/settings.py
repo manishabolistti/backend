158a12a8739c0436df6e555cf8f2bfe22a993206
manisha <manishabolisetti96@gmail.com>
2021-02-09 14:58:00 +0530

    added

--- manisha/manisha/settings.py
+++ manisha/manisha/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'f0wbrb)0zvmjg9b4#5w+fzuiguqehk##ouepf#p%-_cbg!yqmi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
