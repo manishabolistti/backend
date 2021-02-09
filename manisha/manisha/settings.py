105e3d1f09531509d5030cfc940c7a27ec8e396a
manisha <manishabolisetti96@gmail.com>
2021-02-09 15:27:57 +0530

    added

--- manisha/manisha/settings.py
+++ manisha/manisha/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'f0wbrb)0zvmjg9b4#5w+fzuiguqehk##ouepf#p%-_cbg!yqmi'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
